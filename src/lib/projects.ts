88a5924aa3c6d8e91120120e888a76841ff6c161
Aveek-Saha <aveek.s98@gmail.com>
2026-08-17 23:42:26 -0400

    Refine project grids and detail actions

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -14,6 +14,8 @@ export interface Project {
     category: string;
     description?: string;
     client?: string;
+    github?: string;
+    website?: string;
   };
 }
 
@@ -42,4 +44,31 @@ export async function getProjects() {
 
 export function projectPath(project: Project) {
   return `/projects/${project.slug}/`;
+}
+
+function escapeRegExp(value: string) {
+  return value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+export function projectDescription(project: Project) {
+  let description = project.data.description ?? '';
+
+  for (const url of [project.data.github, project.data.website].filter((value): value is string => Boolean(value))) {
+    description = description.replace(
+      new RegExp(`<a\\s+[^>]*href=["']${escapeRegExp(url)}["'][^>]*>.*?<\\/a>`, 'gi'),
+      '',
+    );
+  }
+
+  return description
+    .replace(/(?:Check it out here-?|Check it out on GitHub|Check it out at|Get the code on|Get the code:|Or on GitHub|or read the source code|Or have a look at the|Or fork it on GitHub|Add it to your Discord server here-|Read the)\s*/gi, '')
+    .replace(/(<br\s*\/?>)\s*Or\s+learn/gi, '$1Learn')
+    .replace(/(<br\s*\/?>)\s*Or\s+/gi, '$1')
+    .replace(/(<br\s*\/?>)\s*[!.]+\s*(?=<br\s*\/?>|<div|$)/gi, '$1')
+    .replace(/(?:<br\s*\/?>\s*){2,}/gi, '<br>')
+    .replace(/<br\s*\/?>\s*(?=<div|$)/gi, '')
+    .replace(/\s+([!.])/g, '$1')
+    .replace(/\.{2,}/g, '.')
+    .replace(/\s{2,}/g, ' ')
+    .trim();
 }
